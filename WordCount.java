f013db84ecb013b4f4f3a33235576d6ba519550a
Mohamed Adel Hassan <58120325+MohamedAdelHsn@users.noreply.github.com>
2020-06-18 21:47:30 +0200

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -8,10 +8,10 @@ public class WordCount {
 
 	  // data source that will be matched to count the words
     
-	 String src = "Combined with the below initiative or implemented separately, "
-			+ "developing self-service access and reporting to your data is something that can free up your IT and analytics staff. "
-			+ "Your organization will be much more efficient if any member of the team can build and run a report rather than waiting for a custom report to be created and executed for them. This type of project might feel a bit like ‘dashboards’ but it should be much more than "
-			+ "that – your people should be able to get into the data, see the data and manipulate the data and then build a report or visualization based on those manipulations. Of course, you need a good data governance process in place to ensure that the right people can see the right data";
+    String src = "Combined with the below initiative or implemented separately, "
+	+ "developing self-service access and reporting to your data is something that can free up your IT and analytics staff. "
+        + "Your organization will be much more efficient if any member of the team can build and run a report rather than waiting for a custom report to be created and executed for them. This type of project might feel a bit like ‘dashboards’ but it should be much more than "
+        + "that – your people should be able to get into the data, see the data and manipulate the data and then build a report or visualization based on those manipulations. Of course, you need a good data governance process in place to ensure that the right people can see the right data";
 		
     // seperate data or tokens using whiteSpace \\s
     
